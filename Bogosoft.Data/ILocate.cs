befd2898582e437bd7da3d4c9e0091f489d776d1
Clayton Roth <croth0627@yahoo.com>
2016-07-26 21:49:35 -0700

    Locator Contract
    * Added locator contract.

---
+++ Bogosoft.Data/ILocate.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Bogosoft.Data
+{
+    /// <summary>Provides a means of locating an entity by a unique identifier.</summary>
+    /// <typeparam name="TEntity">The type of the entity being located.</typeparam>
+    /// <typeparam name="TId">The type of the entity's unique identifier.</typeparam>
+    public interface ILocate<TEntity, TId>
+    {
+        /// <summary>Locate an entity by its unique identifier.</summary>
+        /// <param name="id">The unique identifier on which to locate an entity.</param>
+        /// <returns>A task which will eventually return an object of the entity type.</returns>
+        Task<TEntity> LocateAsync(TId id);
+    }
+}
