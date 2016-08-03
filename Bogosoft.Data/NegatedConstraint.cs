f3d767119ffcfd6014386351da9ebe285f99d4fa
Clayton Roth <croth0627@yahoo.com>
2016-08-02 22:23:33 -0700

    Added Constraint Contracts
    * Covers fluently creating validation constraints with the basic boolean operations.

---
+++ Bogosoft.Data/NegatedConstraint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bogosoft.Data
+{
+    internal sealed class NegatedConstraint<T> : IConstrain<T>
+    {
+        private IConstrain<T> constraint;
+
+        internal NegatedConstraint(IConstrain<T> constraint)
+        {
+            this.constraint = constraint;
+        }
+
+        public Boolean Validate(T graph)
+        {
+            return false == this.constraint.Validate(graph);
+        }
+    }
+}
