b8ac33d1d2f1bdf704cb01d269b0d17b015107a9
Clayton Roth <croth0627@yahoo.com>
2016-08-25 20:33:37 -0700

    Added `CopyTo` and `CopyToAsync`.

--- Bogosoft.Data/SearchExtensions.cs
+++ Bogosoft.Data/SearchExtensions.cs
@@ -32,5 +32,62 @@ namespace Bogosoft.Data
 
             return entities ?? new TEntity[0];
         }
+
+        /// <summary>
+        /// Copy a paged search result of a source <see cref="ISearch{TEntity, TParameters}"/> implementation
+        /// to a destination <see cref="ISave{TEntity, TId}"/> implementation. 
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entities to be searched/stored.</typeparam>
+        /// <typeparam name="TId">The type of the entities' unique identifier.</typeparam>
+        /// <typeparam name="TParameters">The type of the parameters to search against.</typeparam>
+        /// <param name="source">The current <see cref="ISearch{TEntity, TParameters}"/> implementation.</param>
+        /// <param name="destination">A target <see cref="ISave{TEntity, TId}"/> implementation.</param>
+        /// <param name="parameters">Parameters to search for entities against.</param>
+        /// <param name="page">The page of results to search/store.</param>
+        /// <param name="limit">The number of results per page.</param>
+        /// <returns>
+        /// A <see cref="Task"/> which will eventually return a collection of the unique identifiers
+        /// corresponding to the successfully-saved entities.
+        /// </returns>
+        public static IEnumerable<TId> CopyTo<TEntity, TId, TParameters>(
+            this ISearch<TEntity, TParameters> source,
+            ISave<TEntity, TId> destination,
+            TParameters parameters,
+            Int32 page = 0,
+            Int32 limit = Int32.MaxValue
+            )
+        {
+            IEnumerable<TId> ids = null;
+
+            Task.Run(async () => ids = await source.CopyToAsync(destination, parameters, page, limit)).Wait();
+
+            return ids ?? new TId[0];
+        }
+
+        /// <summary>
+        /// Copy a paged search result of a source <see cref="ISearch{TEntity, TParameters}"/> implementation
+        /// to a destination <see cref="ISave{TEntity, TId}"/> implementation. 
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entities to be searched/stored.</typeparam>
+        /// <typeparam name="TId">The type of the entities' unique identifier.</typeparam>
+        /// <typeparam name="TParameters">The type of the parameters to search against.</typeparam>
+        /// <param name="source">The current <see cref="ISearch{TEntity, TParameters}"/> implementation.</param>
+        /// <param name="destination">A target <see cref="ISave{TEntity, TId}"/> implementation.</param>
+        /// <param name="parameters">Parameters to search for entities against.</param>
+        /// <param name="page">The page of results to search/store.</param>
+        /// <param name="limit">The number of results per page.</param>
+        /// <returns>
+        /// A collection of the unique identifiers corresponding to the successfully-saved entities.
+        /// </returns>
+        public static async Task<IEnumerable<TId>> CopyToAsync<TEntity, TId, TParameters>(
+            this ISearch<TEntity, TParameters> source,
+            ISave<TEntity, TId> destination,
+            TParameters parameters,
+            Int32 page = 0,
+            Int32 limit = Int32.MaxValue
+            )
+        {
+            return await destination.SaveAsync(await source.SearchAsync(parameters, page, limit));
+        }
     }
 }
