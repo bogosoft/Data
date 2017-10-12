c29309d9ee75ad9bb54867e7578f52d122fa4667
Clayton Roth <croth0627@yahoo.com>
2017-10-11 23:13:39 -0700

    added console test project

---
+++ Bogosoft.Data.ConsoleTest/Hero.cs
@@ -0,0 +1,16 @@
+﻿namespace Bogosoft.Data.ConsoleTest
+{
+    class Hero
+    {
+        internal int Height;
+
+        internal string MostNotableFeat;
+
+        internal string Name;
+
+        public override string ToString()
+        {
+            return $"Introducing, '{Name}', standing {Height} feet tall; most notable for {MostNotableFeat}";
+        }
+    }
+}
