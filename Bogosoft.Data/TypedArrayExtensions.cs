135cf241940bf03dff4a67433552055fc68a7065
Clayton Roth <croth0627@yahoo.com>
2017-10-11 23:13:03 -0700

    added some types and extension methods

---
+++ Bogosoft.Data/TypedArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bogosoft.Data
+{
+    static class TypedArrayExtensions
+    {
+        internal static T[] Copy<T>(this T[] source) => source.Copy(source.Length);
+
+        internal static T[] Copy<T>(this T[] source, int size)
+        {
+            var destination = new T[size];
+
+            Array.Copy(source, destination, size);
+
+            return destination;
+        }
+    }
+}
