1a15394f9fcad5b8783dae5631bd6bc5151cffe5
Clayton Roth <croth0627@yahoo.com>
2017-10-13 00:14:11 -0700

    added integration tests project

---
+++ Bogosoft.Data.Tests/Config.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace Bogosoft.Data.Tests
+{
+    static class Config
+    {
+        internal static class AppSettings
+        {
+            internal static string TestDatabase => ConfigurationManager.AppSettings["TestDatabase"];
+        }
+
+        internal static class ConnectionStrings
+        {
+            internal static string TestConnectionString => ConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString;
+        }
+    }
+}
