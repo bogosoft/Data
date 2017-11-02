a96e77cc4a1c42d729f4793aefc56ce9238fdf44
Clayton Roth <croth0627@yahoo.com>
2017-11-01 22:04:20 -0700

    added prepending data reader

---
+++ Bogosoft.Data.Tests/UnitTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Should;
+using System;
+using System.Collections.Generic;
+
+namespace Bogosoft.Data.Tests
+{
+    [TestFixture, Category("Unit")]
+    public class UnitTests
+    {
+        const string One = "1";
+        const string Two = "2";
+        const string Three = "3";
+
+        static IEnumerable<string[]> Rows
+        {
+            get
+            {
+                yield return new[] { "First", "Second", "Third" };
+                yield return new[] { One, Two, Three };
+            }
+        }
+
+        [TestCase]
+        public void PrependedDataReaderReturnsNewValueFromFirstOrdinalPosition()
+        {
+            using (var reader = RowSequenceDataReader.WithFieldNamesOnFirstLine(Rows))
+            {
+                reader.Read().ShouldBeTrue();
+
+                reader.GetFieldValue<string>(0).ShouldEqual(One);
+            }
+
+            var added = DateTime.UtcNow.ToString();
+
+            using (var reader = RowSequenceDataReader.WithFieldNamesOnFirstLine(Rows).Prepend("Zeroeth", added))
+            {
+                reader.Read().ShouldBeTrue();
+
+                reader.GetFieldValue<string>(0).ShouldEqual(added);
+                reader.GetFieldValue<string>(1).ShouldEqual(One);
+            }
+        }
+    }
+}
---
+++ Bogosoft.Data/DbDataReaderExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Common;
+
+namespace Bogosoft.Data
+{
+    /// <summary>
+    /// Extended functionality for the <see cref="DbDataReader"/> type.
+    /// </summary>
+    public static class DbDataReaderExtensions
+    {
+        /// <summary>
+        /// Return a new data reader that prepends a given value to the first ordinal
+        /// position of every read row. Ordinal positions of the underlying data reader
+        /// will be mapped as n + 1.
+        /// </summary>
+        /// <typeparam name="T">The type of the value to prepend.</typeparam>
+        /// <param name="source">The current data reader.</param>
+        /// <param name="name">
+        /// A value corresponding to the name of the field to prepend.
+        /// </param>
+        /// <param name="value">A value to prepend.</param>
+        /// <returns>
+        /// The current data reader decorated with a value prepender.
+        /// </returns>
+        public static DbDataReader Prepend<T>(this DbDataReader source, string name, T value)
+        {
+            return new PrependingDataReader<T>(source, name, x => value);
+        }
+
+        /// <summary>
+        /// Returns a new data reader that prepends the result of calling a given delegate
+        /// to the first ordinal position of every read row. Ordinal positions of the
+        /// underlying data reader will be mapped as n + 1.
+        /// </summary>
+        /// <typeparam name="T">The type of the value to prepend.</typeparam>
+        /// <param name="source">The current data reader.</param>
+        /// <param name="name">
+        /// A value corresponding to the name of the field to prepend.
+        /// </param>
+        /// <param name="computor">A delegate which will provide the value to prepend.</param>
+        /// <returns>
+        /// The current data reader decorated with a value provider prepender.
+        /// </returns>
+        public static DbDataReader Prepend<T>(this DbDataReader source, string name, ValueComputor<T> computor)
+        {
+            return new PrependingDataReader<T>(source, name, computor);
+        }
+    }
+}
