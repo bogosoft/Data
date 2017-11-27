904c6753fb4f1d116dbfdd4ee1ee1c36c1157459
Clayton Roth <croth0627@yahoo.com>
2017-11-26 23:14:22 -0800

    added `ParsingDataReader` for string array sequences

--- Bogosoft.Data/ArraySequenceExtensions.cs
+++ Bogosoft.Data/ArraySequenceExtensions.cs
@@ -33,7 +33,7 @@ namespace Bogosoft.Data
 
             if (!records.MoveNext())
             {
-                throw new InvalidOperationException("Sequence contains zero records.");
+                throw new InvalidOperationException(Messages.NoRecords);
             }
 
             return records.ToDataReader(records.Current.Select(x => x is string ? x as string : x.ToString()));
@@ -100,6 +100,131 @@ namespace Bogosoft.Data
         public static DbDataReader ToDataReader(this IEnumerable<object[]> records, IEnumerable<string> columns)
         {
             return records.GetEnumerator().ToDataReader(columns);
+        }
+
+        /// <summary>
+        /// Convert the current record enumerator into a data reader. Column names
+        /// will be taken from the first record retrieved from the enumerator.
+        /// </summary>
+        /// <param name="records">The current sequence of records.</param>
+        /// <param name="parsers">
+        /// A sequence of parsers to use for converting string values to objects. The ordinal position of a
+        /// parser within the sequence will correspond exactly to the ordinal position of the value to be
+        /// parsed within each record.
+        /// </param>
+        /// <returns>
+        /// The current record enumerator wrapped in a data reader.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thronw in the event that the given enumerator is null.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown in the event that the given enumerator contains no records.
+        /// </exception>
+        public static DbDataReader ToDataReader(this IEnumerator<string[]> records, IEnumerable<Parser> parsers)
+        {
+            if (records is null)
+            {
+                throw new ArgumentNullException(nameof(records));
+            }
+
+            if (!records.MoveNext())
+            {
+                throw new InvalidOperationException(Messages.NoRecords);
+            }
+
+            return records.ToDataReader(records.Current, parsers);
+        }
+
+        /// <summary>
+        /// Convert the current record enumerator into a data reader.
+        /// </summary>
+        /// <param name="records">The current sequence of records.</param>
+        /// <param name="columns">
+        /// A sequence of column names. The ordinality of each column will correspond
+        /// to the position of the column name within the given sequence.
+        /// </param>
+        /// <param name="parsers">
+        /// A sequence of parsers to use for converting string values to objects. The ordinal position of a
+        /// parser within the sequence will correspond exactly to the ordinal position of the value to be
+        /// parsed within each record.
+        /// </param>
+        /// <returns>
+        /// The current record enumerator wrapped in a data reader.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thronw in the event that the given enumerator or the given column name sequence is null.
+        /// </exception>
+        public static DbDataReader ToDataReader(
+            this IEnumerator<string[]> records,
+            IEnumerable<string> columns,
+            IEnumerable<Parser> parsers
+            )
+        {
+            if (records is null)
+            {
+                throw new ArgumentNullException(nameof(records));
+            }
+
+            if (columns is null)
+            {
+                throw new ArgumentNullException(nameof(columns));
+            }
+
+            if (parsers is null)
+            {
+                throw new ArgumentNullException(nameof(parsers));
+            }
+
+            return new ParsingDataReader(records, columns.ToArray(), parsers.ToArray());
+        }
+
+        /// <summary>
+        /// Convert the current sequence of records into a data reader. Column names
+        /// will be taken from the first record retrieved from the sequence.
+        /// </summary>
+        /// <param name="records">The current sequence of records.</param>
+        /// <param name="parsers">
+        /// A sequence of parsers to use for converting string values to objects. The ordinal position of a
+        /// parser within the sequence will correspond exactly to the ordinal position of the value to be
+        /// parsed within each record.
+        /// </param>
+        /// <returns>The current sequence of records wrapped in a data reader.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thronw in the event that the given sequene is null.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown in the event that the given sequence contains no records.
+        /// </exception>
+        public static DbDataReader ToDataReader(this IEnumerable<string[]> records, IEnumerable<Parser> parsers)
+        {
+            return records.GetEnumerator().ToDataReader(parsers);
+        }
+
+        /// <summary>
+        /// Convert the current sequence of records into a data reader.
+        /// </summary>
+        /// <param name="records">The current sequence of records.</param>
+        /// <param name="columns">
+        /// A sequence of column names. The ordinality of each column will correspond
+        /// to the position of the column name within the given sequence.
+        /// </param>
+        /// <param name="parsers">
+        /// A sequence of parsers to use for converting string values to objects. The ordinal position of a
+        /// parser within the sequence will correspond exactly to the ordinal position of the value to be
+        /// parsed within each record.
+        /// </param>
+        /// <returns>The current sequence of records wrapped in a data reader.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thronw in the event that the given sequence of records or sequence of column names is null.
+        /// </exception>
+        public static DbDataReader ToDataReader(
+            this IEnumerable<string[]> records,
+            IEnumerable<string> columns,
+            IEnumerable<Parser> parsers
+            )
+        {
+            return records.GetEnumerator().ToDataReader(columns, parsers);
         }
     }
 }
---
+++ Bogosoft.Data/Messages.cs
@@ -0,0 +1,20 @@
+﻿namespace Bogosoft.Data
+{
+    /// <summary>
+    /// A set of messages related to data readers.
+    /// </summary>
+    public static class Messages
+    {
+        /// <summary>
+        /// Get a message indicating that zero records were encountered where
+        /// at least one record was expected.
+        /// </summary>
+        public const string NoRecords = "Sequence contains zero records.";
+
+        /// <summary>
+        /// Get a message indicating that the number of parsers in a given sequence was
+        /// less than the number of columns in a given sequence.
+        /// </summary>
+        public const string TooFewParsers = "The number of given parsers cannot be less than the number of given columns";
+    }
+}
