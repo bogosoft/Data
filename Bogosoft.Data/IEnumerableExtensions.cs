5c1bf0d1aec8316bc685b337ed0b6f2492c95cad
Clayton Roth <croth0627@yahoo.com>
2019-10-17 18:12:40 -0600

    refactored collection to data reader adapter

--- Bogosoft.Data/IEnumerableExtensions.cs
+++ Bogosoft.Data/IEnumerableExtensions.cs
@@ -10,55 +10,56 @@ namespace Bogosoft.Data
     /// </summary>
     public static class IEnumerableExtensions
     {
-        /// <summary>
-        /// Convert the current enumerable sequence to a .NET data reader.
-        /// </summary>
-        /// <typeparam name="T">The type of the items in the current sequence.</typeparam>
-        /// <param name="items">The current <see cref="IEnumerable{T}"/> implementation.</param>
-        /// <param name="fields">
-        /// A sequence of field adapters for the new data record. The ordinal position of each field in the sequence
-        /// will populate the corresponding ordinal position in a record with its value extractor.
-        /// </param>
-        /// <returns>A new data reader.</returns>
-        public static DbDataReader ToDataReader<T>(this IEnumerable<T> items, IEnumerable<FieldAdapter<T>> fields)
+        static DbDataReader CreateDataReader<T>(IEnumerable<T> source, FieldAdapter<T>[] fields)
         {
-            return ToDataReader(items.GetEnumerator(), fields.ToArray());
-        }
-
-        /// <summary>
-        /// Convert the current enumerable sequence to a .NET data reader.
-        /// </summary>
-        /// <typeparam name="T">The type of the items in the current sequence.</typeparam>
-        /// <param name="items">The current <see cref="IEnumerable{T}"/> implementation.</param>
-        /// <param name="fields">
-        /// A sequence of field adapters for the new data record. The ordinal position of each field in the sequence
-        /// will populate the corresponding ordinal position in a record with its value extractor.
-        /// </param>
-        /// <returns>A new data reader.</returns>
-        public static DbDataReader ToDataReader<T>(this IEnumerable<T> items, params FieldAdapter<T>[] fields)
-        {
-            return ToDataReader(items.GetEnumerator(), fields);
-        }
-
-        static DbDataReader ToDataReader<T>(this IEnumerator<T> source, FieldAdapter<T>[] fields)
-        {
-            var reader = new CollectionToDataReaderAdapter<T>
-            {
-                Buffer = new object[fields.Length],
-                FieldIndicesByName = new Dictionary<string, int>(),
-                Fields = fields,
-                SchemaTable = new DataTable(),
-                Source = source
-            };
+            var buffer = new object[fields.Length];
+            var fieldIndicesByName = new Dictionary<string, int>();
+            var schemaTable = new DataTable();
 
             for (var i = 0; i < fields.Length; i++)
             {
-                reader.FieldIndicesByName[fields[i].Name] = i;
+                fieldIndicesByName[fields[i].Name] = i;
 
-                reader.SchemaTable.Columns.Add(fields[i].Name, fields[i].Type);
+                schemaTable.Columns.Add(fields[i].Name, fields[i].Type);
             }
 
-            return reader;
+            return new SynchronousCollectionToDataReaderAdapter<T>(
+                buffer,
+                fieldIndicesByName,
+                fields.ToArray(),
+                schemaTable,
+                source
+                );
+        }
+
+        /// <summary>
+        /// Convert the current enumerable sequence to a .NET data reader.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the current sequence.</typeparam>
+        /// <param name="source">The current sequence of items.</param>
+        /// <param name="fields">
+        /// A sequence of field adapters for the new data record. The ordinal position of each field in the sequence
+        /// will populate the corresponding ordinal position in a record with its value extractor.
+        /// </param>
+        /// <returns>A new data reader.</returns>
+        public static DbDataReader ToDataReader<T>(this IEnumerable<T> source, IEnumerable<FieldAdapter<T>> fields)
+        {
+            return CreateDataReader(source, fields.ToArray());
+        }
+
+        /// <summary>
+        /// Convert the current enumerable sequence to a .NET data reader.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the current sequence.</typeparam>
+        /// <param name="source">The current sequence of items.</param>
+        /// <param name="fields">
+        /// A sequence of field adapters for the new data record. The ordinal position of each field in the sequence
+        /// will populate the corresponding ordinal position in a record with its value extractor.
+        /// </param>
+        /// <returns>A new data reader.</returns>
+        public static DbDataReader ToDataReader<T>(this IEnumerable<T> source, params FieldAdapter<T>[] fields)
+        {
+            return CreateDataReader(source, fields);
         }
     }
 }
